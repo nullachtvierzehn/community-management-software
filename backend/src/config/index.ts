8c734d718dff4ee9d82cca6a7716750c93da6ca2
Timo Stolz <timo.stolz@gmail.com>
2023-11-24 17:50:02 +0100

    check config in backend when starting the app

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,9 +1,11 @@
-import {config as loadConfig} from 'dotenv'
-import findConfig from 'find-config'
+import { config as loadConfig } from "dotenv";
+import findConfig from "find-config";
+import { DeepPartial } from "../utils/deep-partial.js";
+import { partial } from "lodash";
 
-const configPath = findConfig('.env')
-if(!configPath) throw new Error('no .env found!')
-loadConfig({ path: configPath })
+const configPath = findConfig(".env");
+if (!configPath) throw new Error("no .env found!");
+loadConfig({ path: configPath });
 
 //import local from "./local.json" assert { type: "json" };
 
@@ -25,27 +27,71 @@ export interface Config {
   };
 }
 
-//export default local as unknown as Config;
-
-export const config: Config = {
-  database: {
-    rootUrl: process.env.ROOT_DATABASE_URL,
-    host: process.env.DATABASE_HOST as string,
-    name: process.env.DATABASE_NAME as string,
-    roles: {
-      owner: {
-        username: process.env.DATABASE_OWNER as string,
-        password: process.env.DATABASE_OWNER_PASSWORD as string
-      },
-      authenticator: {
-        username: process.env.DATABASE_AUTHENTICATOR as string,
-        password: process.env.DATABASE_AUTHENTICATOR_PASSWORD as string
-      },
-      visitor: {
-        username: process.env.DATABASE_VISITOR as string
+export function isValidConfig(
+  particalConfig: DeepPartial<Config>
+): particalConfig is Config {
+  const missingProperties = [];
+  if (!particalConfig.database) {
+    missingProperties.push("database");
+  } else {
+    if (!particalConfig.database.host) missingProperties.push("database.host");
+    if (!particalConfig.database.name) missingProperties.push("database.name");
+    if (!particalConfig.database.roles) {
+      missingProperties.push("database.roles");
+    } else {
+      if (!particalConfig.database.roles.authenticator) {
+        missingProperties.push("database.roles.authenticator");
+      } else {
+        if (!particalConfig.database.roles.authenticator.username)
+          missingProperties.push("database.roles.authenticator.username");
+        if (!particalConfig.database.roles.authenticator.password)
+          missingProperties.push("database.roles.authenticator.password");
+      }
+      if (!particalConfig.database.roles.owner) {
+        missingProperties.push("database.roles.owner");
+      } else {
+        if (!particalConfig.database.roles.owner.username)
+          missingProperties.push("database.roles.owner.username");
+        if (!particalConfig.database.roles.owner.password)
+          missingProperties.push("database.roles.owner.password");
+      }
+      if (!particalConfig.database.roles.visitor) {
+        missingProperties.push("database.roles.visitor");
+      } else {
+        if (!particalConfig.database.roles.visitor.username)
+          missingProperties.push("database.roles.visitor.username");
       }
     }
   }
-} 
+  if (missingProperties.length > 0) {
+    console.error("missing config attributes", missingProperties);
+    return false;
+  } else {
+    return true;
+  }
+}
 
-export default config
+export const config: DeepPartial<Config> = {
+  database: {
+    rootUrl: process.env.ROOT_DATABASE_URL,
+    host: process.env.DATABASE_HOST,
+    name: process.env.DATABASE_NAME,
+    roles: {
+      owner: {
+        username: process.env.DATABASE_OWNER,
+        password: process.env.DATABASE_OWNER_PASSWORD,
+      },
+      authenticator: {
+        username: process.env.DATABASE_AUTHENTICATOR,
+        password: process.env.DATABASE_AUTHENTICATOR_PASSWORD,
+      },
+      visitor: {
+        username: process.env.DATABASE_VISITOR,
+      },
+    },
+  },
+};
+
+if (!isValidConfig(config)) throw new Error("invalid config");
+
+export default config as Config;
