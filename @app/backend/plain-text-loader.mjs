bcf1b98d1a14f2556fc7c4e1c1a44d92273b493f
Timo Stolz <timo.stolz@gmail.com>
2023-12-09 14:49:01 +0100

    lint:fix plain-text-loader.mjs

--- @app/backend/plain-text-loader.mjs
+++ @app/backend/plain-text-loader.mjs
@@ -1,19 +1,18 @@
-import { promises as fs } from "fs";
-import { pathToFileURL } from "url";
-import { fileURLToPath } from "url";
+import { promises as fs } from 'fs'
+import { fileURLToPath } from 'url'
 
 async function load(url, context, defaultLoad) {
-  if (url.endsWith(".txt") || url.endsWith(".mjml")) {
-    const filePath = fileURLToPath(url);
-    const content = await fs.readFile(filePath, "utf8");
+  if (url.endsWith('.txt') || url.endsWith('.mjml')) {
+    const filePath = fileURLToPath(url)
+    const content = await fs.readFile(filePath, 'utf8')
     return {
-      format: "module",
+      format: 'module',
       source: `export default ${JSON.stringify(content)}`,
       shortCircuit: true, // Signal that the loader chain should end here
-    };
+    }
   }
   // Fallback to default loader for other files
-  return defaultLoad(url, context, defaultLoad);
+  return defaultLoad(url, context, defaultLoad)
 }
 
-export { load };
+export { load }
