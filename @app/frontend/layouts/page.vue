552ef189d11a02d6b95edee4be217e03acb91e2d
Timo Stolz <timo.stolz@nullachtvierzehn.de>
2023-12-27 16:07:53 +0100

    add /layouts to frontend

---
+++ @app/frontend/layouts/page.vue
@@ -0,0 +1,11 @@
+<template>
+  <article class="container mx-auto p-4">
+    <slot></slot>
+  </article>
+</template>
+
+<style lang="postcss">
+article h1 {
+  @apply text-4xl font-bold mb-4;
+}
+</style>
