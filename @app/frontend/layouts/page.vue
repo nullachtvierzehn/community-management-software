45a7d2434640e39121f72f1abf2fe1f5e8176986
Timo Stolz <timo.stolz@gmail.com>
2023-12-29 08:06:21 +0100

    improve styles in page layout

--- @app/frontend/layouts/page.vue
+++ @app/frontend/layouts/page.vue
@@ -9,6 +9,26 @@ article :deep(h1) {
   @apply text-4xl font-bold mb-4;
 }
 
+article :deep(h2) {
+  @apply text-3xl font-bold mb-3;
+}
+
+article :deep(h3) {
+  @apply text-2xl font-bold mb-2;
+}
+
+article :deep(section h1) {
+  @apply text-3xl font-bold mb-4;
+}
+
+article :deep(section h2) {
+  @apply text-2xl font-bold mb-3;
+}
+
+article :deep(section h3) {
+  @apply text-xl font-bold mb-2;
+}
+
 article :deep(p) {
   @apply my-4;
 }
