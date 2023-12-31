932365134c29d2e6936561bed3c0905f4dc35c12
Timo Stolz <timo.stolz@nullachtvierzehn.de>
2023-12-31 14:12:21 +0100

    fix app.run_worker() later

--- @app/python-worker/python_worker/__main__.py
+++ @app/python-worker/python_worker/__main__.py
@@ -6,4 +6,5 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format=logging.BASIC_FORMAT, style="%")
 
 with app.open():
+    # FIXME: must be restarted when database shuts down
     app.run_worker(wait=True)
