6bec9fee2a048858962f301d9dc8cae01cb8a18e
Timo Stolz <timo.stolz@gmail.com>
2024-03-14 16:07:33 +0100

    show spaces if public

---
+++ @app/database/schema/deploy/space-submissions/policies/select-own.sql
@@ -0,0 +1,9 @@
+-- Deploy 0814-cms:space-submissions/policies/select-own to pg
+-- requires: spaces/my-space-ids
+-- requires: space-submissions
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ @app/database/schema/revert/space-submissions/policies/select-own.sql
@@ -0,0 +1,7 @@
+-- Revert 0814-cms:space-submissions/policies/select-own from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
