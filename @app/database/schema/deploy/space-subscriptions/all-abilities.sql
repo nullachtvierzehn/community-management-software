bbf7f649fa229d13002e24c0f1ba0d29318c1bf3
Timo Stolz <timo.stolz@gmail.com>
2024-03-21 16:25:20 +0100

    fix all_abilities

--- @app/database/schema/deploy/space-subscriptions/all-abilities.sql
+++ @app/database/schema/deploy/space-subscriptions/all-abilities.sql
@@ -3,7 +3,7 @@
 
 BEGIN;
 
-create or replace function app_public.space_subscriptions_all_abilities(s app_public.space_items)
+create or replace function app_public.space_subscriptions_all_abilities(s app_public.space_subscriptions)
 returns app_public.ability[]
 language sql
 stable
@@ -16,12 +16,12 @@ as $$
     and user_id = app_public.current_user_id()
 $$;
 
-comment on function app_public.space_subscriptions_all_abilities(s app_public.space_items) is $$
-  @behavior +filterBy +typeField
-  $$;
+grant execute on function app_public.space_subscriptions_all_abilities(s app_public.space_subscriptions) to "$DATABASE_VISITOR";
+
+comment on function app_public.space_subscriptions_all_abilities(s app_public.space_subscriptions) is E'@behavior +typeField +filterBy +proc:filterBy';
 
 
-create or replace function app_public.space_subscriptions_all_abilities_with_grant_option(s app_public.space_items)
+create or replace function app_public.space_subscriptions_all_abilities_with_grant_option(s app_public.space_subscriptions)
 returns app_public.ability[]
 language sql
 stable
@@ -34,8 +34,8 @@ as $$
     and user_id = app_public.current_user_id()
 $$;
 
-comment on function app_public.space_subscriptions_all_abilities_with_grant_option(s app_public.space_items) is $$
-  @behavior +filterBy +typeField
-  $$;
+grant execute on function app_public.space_subscriptions_all_abilities_with_grant_option(s app_public.space_subscriptions) to "$DATABASE_VISITOR";
+
+comment on function app_public.space_subscriptions_all_abilities_with_grant_option(s app_public.space_subscriptions) is E'@behavior +typeField +filterBy +proc:filterBy';
 
 COMMIT;
--- @app/database/schema/revert/space-subscriptions/all-abilities.sql
+++ @app/database/schema/revert/space-subscriptions/all-abilities.sql
@@ -2,8 +2,8 @@
 
 BEGIN;
 
-drop function app_public.space_subscriptions_all_abilities(s app_public.space_items);
+drop function app_public.space_subscriptions_all_abilities(s app_public.space_subscriptions);
 
-drop function app_public.space_subscriptions_all_abilities_with_grant_option(s app_public.space_items);
+drop function app_public.space_subscriptions_all_abilities_with_grant_option(s app_public.space_subscriptions);
 
 COMMIT;
