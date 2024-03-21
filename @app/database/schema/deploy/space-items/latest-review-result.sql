6a78a5b37474de85cdab8c14dc437527a6c5c188
Timo Stolz <timo.stolz@gmail.com>
2024-03-21 21:53:41 +0100

    fix latest-review-result

--- @app/database/schema/deploy/space-items/latest-review-result.sql
+++ @app/database/schema/deploy/space-items/latest-review-result.sql
@@ -3,6 +3,20 @@
 
 BEGIN;
 
--- XXX Add DDLs here.
+create or replace function app_public.space_items_latest_review_result(i app_public.space_items)
+returns app_public.review_result
+language sql
+stable
+parallel safe
+as $$
+  select review_result
+  from app_hidden.space_item_submissions_and_reviews
+  where
+    item_id = i.id
+    and submission_is_reviewed
+    and submission_is_latest_active
+$$;
+
+comment on function app_public.space_items_latest_review_result(i app_public.space_items) is E'@behavior +filterBy +orderBy +typeField';
 
 COMMIT;
